2f88a3782b3c4538891aaaefc214ce3fcce028eb
Ben Longman <bmfrosty@catbus.org>
2018-05-22 18:26:38 -0700

    reduction in compress makes loading faster on mobile.  Promote from test to prod instead of uploading twice.

--- upload.sh
+++ upload.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-case "$1" in
-        test)
-            BUCKET=test.al-legal.com
-            ;;
-        prod)
-            BUCKET=al-legal.com
-            ;;
-        *)
-            echo "Usage: $0 {test|prod}"
-            exit 1
-esac
+BUCKET=test.al-legal.com
 
 pandoc --template other/template.html -s -i site/*md       | gzip -1 | aws s3 cp - s3://$BUCKET/index.html --acl public-read --content-type text/html --content-encoding gzip
-pandoc --template other/template.html -s -i other/error.md | gzip -9 | aws s3 cp - s3://$BUCKET/error.html --acl public-read --content-type text/html --content-encoding gzip
+pandoc --template other/template.html -s -i other/error.md | gzip -1 | aws s3 cp - s3://$BUCKET/error.html --acl public-read --content-type text/html --content-encoding gzip
